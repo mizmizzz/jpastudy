091ff5aa49713409bb65a7c57040cb645e0c4bac
munjieun <mjsu10@naver.com>
2024-06-25 09:45:58 +0900

    #initial commit

---
+++ src/main/java/com/spring/jpacstudy/JpacstudyApplication.java
@@ -0,0 +1,13 @@
+package com.spring.jpacstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpacstudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpacstudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/jpacstudy/JpacstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.jpacstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpacstudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
